9e934d0412bbb21803ade954704aec219b54d5a2
Ravil Galeyev <dehasi@yandex.ru>
2018-02-09 21:46:37 +0100

    implement pca svd

--- mlng/machine-learning-ex7/ex7/pca.m
+++ mlng/machine-learning-ex7/ex7/pca.m
@@ -8,8 +8,8 @@ function [U, S] = pca(X)
 [m, n] = size(X);
 
 % You need to return the following variables correctly.
-U = zeros(n);
-S = zeros(n);
+%U = zeros(n);
+%S = zeros(n);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should first compute the covariance matrix. Then, you
@@ -21,10 +21,8 @@ S = zeros(n);
 %
 
 
-
-
-
-
+Sigma = (X' * X) / m;
+[U S V] = svd(Sigma);
 
 % =========================================================================
 
