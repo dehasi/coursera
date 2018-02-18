5ec16c3be3563553d7f790f1e32cae217fa24a4a
Ravil Galeyev <dehasi@yandex.ru>
2018-02-18 21:05:29 +0100

    implement collaborative gradient regualrisation

--- mlng/machine-learning-ex8/ex8/cofiCostFunc.m
+++ mlng/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,8 +40,6 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-reg_th = 0;
-reg_x = 0;
 for i = 1:num_movies
     for j = 1:num_users
         if R(i,j) == 0
@@ -61,16 +59,15 @@ for i = 1:num_movies
     idx = find(R(i, :)==1);
     Theta_temp = Theta(idx, :);
     Y_temp = Y(i, idx);
-    X_grad(i, :) = (X(i, :) * Theta_temp' - Y_temp) * Theta_temp;
+    X_grad(i, :) = (X(i, :) * Theta_temp' - Y_temp) * Theta_temp + lambda*X(i, :);
 end
-%
+
 for j = 1:num_users
     idx = find(R(:, j)==1);
-    X_temp = X( idx,:);
-    Y_temp = Y( idx,j); 
-    Theta_grad( j,:) = (X_temp * Theta(j,:)' - Y_temp)' * X_temp;
+    X_temp = X(idx,:);
+    Y_temp = Y(idx,j); 
+    Theta_grad(j,:) = (X_temp * Theta(j,:)' - Y_temp)' * X_temp + lambda*Theta(j,:);
 end
-%
 
 grad = [X_grad(:); Theta_grad(:)];
 
