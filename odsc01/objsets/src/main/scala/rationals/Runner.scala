d3fcb5d2b5dbcd942823591063666f3bf2ff4a90
Ravil Galeyev <dehasi@yandex.ru>
2018-03-08 22:32:54 +0100

    ODSC01-02: Rewrite to infix

--- odsc01/objsets/src/main/scala/rationals/Runner.scala
+++ odsc01/objsets/src/main/scala/rationals/Runner.scala
@@ -7,10 +7,10 @@ object Runner {
     val x = new Rational(1, 3)
     val y = new Rational(5, 7)
     val z = new Rational(3, 2)
-    println(x.add(y))
-    println(x.sub(y).sub(z))
-    println(x.less(y))
-    println(x.max(y))
+    println(x add y )
+    println(x sub y sub z)
+    println(x less y)
+    println(x max y)
   }
 
 }
