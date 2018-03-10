ce19fadf7488d10abed899c99394caaf9f65b779
Ravil Galeyev <dehasi@yandex.ru>
2018-03-10 18:00:12 +0100

    ODSC01-03: Implement non empty

--- odsc01/objsets/src/main/scala/intset/NonEmpty.scala
+++ odsc01/objsets/src/main/scala/intset/NonEmpty.scala
@@ -1,8 +1,14 @@
 package intset
 
 /** Created by Ravil on 10/03/2018. */
-class NonEmpty(elem:Int, left:IntSet, right:IntSet) extends IntSet {
-  override def incl(x: Int): IntSet = ???
+class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
+  override def contains(x: Int): Boolean =
+    if (elem < x) left contains x
+    else if (x > elem) right contains x
+    else true
 
-  override def contains(x: Int): Boolean = ???
+  override def incl(x: Int): IntSet =
+    if (x < elem) new NonEmpty(elem, left incl x, right)
+    else if (x > elem) new NonEmpty(elem, left, right incl x)
+    else this
 }
