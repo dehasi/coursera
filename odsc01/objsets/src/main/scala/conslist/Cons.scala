a08b10b98e9e2e2325c74f9c4d1d005ac7c3df80
Ravil Galeyev <dehasi@yandex.ru>
2018-03-10 19:32:50 +0100

    ODSC01-03: Add IntList

---
+++ odsc01/objsets/src/main/scala/conslist/Cons.scala
@@ -0,0 +1,6 @@
+package conslist
+
+/** Created by Ravil on 10/03/2018. */
+class Cons(val  head: Int, val tail: IntList) extends IntList {
+
+}
---
+++ odsc01/objsets/src/main/scala/conslist/IntList.scala
@@ -0,0 +1,6 @@
+package conslist
+
+/** Created by Ravil on 10/03/2018. */
+trait IntList {
+
+}
