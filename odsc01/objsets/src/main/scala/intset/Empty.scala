350a5ae423bee7a0d6e50438a6b1d90e2b5ae7ec
Ravil Galeyev <dehasi@yandex.ru>
2018-03-10 18:10:38 +0100

    ODSC01-03: Make empty an object

--- odsc01/objsets/src/main/scala/intset/Empty.scala
+++ odsc01/objsets/src/main/scala/intset/Empty.scala
@@ -1,9 +1,10 @@
 package intset
 
 /** Created by Ravil on 10/03/2018. */
-class Empty extends IntSet {
+object Empty extends IntSet {
   override def contains(x: Int): Boolean = false
-  override def incl(x: Int): IntSet = new NonEmpty(x, new Empty, new Empty);
+
+  override def incl(x: Int): IntSet = new NonEmpty(x, Empty, Empty);
 
   override def toString: String = "."
 }
