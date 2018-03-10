4dff443319cb13c1bca6fdd305630edb07c34a34
Ravil Galeyev <dehasi@yandex.ru>
2018-03-10 19:40:45 +0100

    ODSC01-03: Implement the generic List

--- odsc01/objsets/src/main/scala/conslist/Cons.scala
+++ odsc01/objsets/src/main/scala/conslist/Cons.scala
@@ -2,5 +2,5 @@ package conslist
 
 /** Created by Ravil on 10/03/2018. */
 class Cons[T](val  head: T, val tail: List[T]) extends List[T] {
-
+  override def isEmpty: Boolean = false
 }
--- odsc01/objsets/src/main/scala/conslist/Nil.scala
+++ odsc01/objsets/src/main/scala/conslist/Nil.scala
@@ -2,5 +2,9 @@ package conslist
 
 /** Created by Ravil on 10/03/2018. */
 class Nil[T] extends List[T] {
+  override def isEmpty: Boolean = true
 
+  override def head: T = throw new NoSuchElementException("Nil.head")
+
+  override def tail: List[T] = throw new NoSuchElementException("Nil.tail")
 }
