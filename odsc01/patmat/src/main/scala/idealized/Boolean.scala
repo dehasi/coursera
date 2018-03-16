6c5cc40fff925f4f08f830e4d7e14dd090b1346a
Ravil Galeyev <ravil.galeyev@grandparade.co.uk>
2018-03-16 21:33:33 +0100

    ODSC01-04: Add idealized boolean less operation

--- odsc01/patmat/src/main/scala/idealized/Boolean.scala
+++ odsc01/patmat/src/main/scala/idealized/Boolean.scala
@@ -1,13 +1,20 @@
 package idealized
 
 abstract class Boolean {
-  val  True = null
-  val  False = null
   def ifThenElse[T](t: => T, e: => T): T
   def && (x: => Boolean) : Boolean = ifThenElse(x, False)
   def || (x: => Boolean) : Boolean = ifThenElse(True, x)
   def unary_!  : Boolean = ifThenElse(False, True)
   def == (x: => Boolean) : Boolean = ifThenElse(x, x.unary_!)
   def != (x: => Boolean) : Boolean = ifThenElse(x.unary_!, x)
+  def  < (x: => Boolean) : Boolean = ifThenElse(False, x)
 
+}
+
+object True extends Boolean {
+  override def ifThenElse[T](t: => T, e: => T) = t
+}
+
+object False extends Boolean {
+  override def ifThenElse[T](t: => T, e: => T) = e
 }
