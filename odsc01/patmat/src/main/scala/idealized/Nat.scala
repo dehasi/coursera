2d6c14bf050e6cf1ce557be4ca6f5505d58c1d23
Ravil Galeyev <ravil.galeyev@grandparade.co.uk>
2018-03-17 17:43:18 +0100

    ODSC01-04: Implement Nat

--- odsc01/patmat/src/main/scala/idealized/Nat.scala
+++ odsc01/patmat/src/main/scala/idealized/Nat.scala
@@ -1,32 +1,41 @@
 package idealized
 
 abstract class Nat {
-  def isZero:Boolean
+  def isZero: Boolean
+
   def predecessor: Nat
+
   def successor: Nat
-  def + (that: Nat) : Nat
-  def - (that: Nat) : Nat
+
+  def +(that: Nat): Nat
+
+  def -(that: Nat): Nat
 }
+
 object Zero extends Nat {
   override def isZero = true
 
-  override def predecessor = ???
+  override def predecessor = throw new NoSuchElementException
 
   override def successor = new Succ(Zero)
 
-  override def +(that: Nat) = ???
+  override def +(that: Nat) = that
 
-  override def -(that: Nat) = ???
+  override def -(that: Nat) = throw new NoSuchElementException
 }
 
-class Succ(n:Nat) extends Nat{
+class Succ(n: Nat) extends Nat {
   override def isZero = false
 
-  override def predecessor = ???
+  override def predecessor = this - new Succ(Zero)
 
   override def successor = new Succ(this)
 
-  override def +(that: Nat) = ???
+  override def +(that: Nat) =
+    if (that.isZero) this
+    else this.successor + that.predecessor
 
-  override def -(that: Nat) = ???
+  override def -(that: Nat) =
+    if (that.isZero) this
+    else this.predecessor - that.predecessor
 }
