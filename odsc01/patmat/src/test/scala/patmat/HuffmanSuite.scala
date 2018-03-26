871ea1e77a5160a2f70a8e8ce0e815490e49c3fd
Ravil Galeyev <dehasi@yandex.ru>
2018-03-26 21:54:30 +0200

    ODSC01-04: Add test to codeBits

--- odsc01/patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ odsc01/patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -67,12 +67,21 @@ class HuffmanSuite extends FunSuite {
       assert(list === List(Fork(Fork(Leaf('e', 1), Leaf('t', 2), List('e', 't'), 3), Leaf('x', 4), List('e', 't', 'x'), 7)))
     }
   }
-  test("decode(codeTree, ___)") {
+  test("decode(codeTree, 111) == B") {
     new TestTrees {
       private val codeTree: CodeTree = createCodeTree(string2Chars("AAAAAAAABBBCDEFGH"))
 
       private val chars: List[Char] = decode(codeTree, List(1, 1,1))
       assert(chars === List('B'))
+    }
+  }
+
+  test("codeBits(codeTable, b) == 111") {
+    new TestTrees {
+      private val codeTable: CodeTable = List(('a', List(1,0,1)),('b', List(1,1,1)),('c', List(0,0,1)))
+      private val charToBits: Char => List[Bit] = codeBits(codeTable)
+
+      assert(charToBits('b') === List(1,1,1))
     }
   }
 
