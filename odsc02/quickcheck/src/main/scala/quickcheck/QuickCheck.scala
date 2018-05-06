0fce04ba02f8f0f5dac727f9d2b51ec934dff057
Ravil Galeyev <ravil.galeyev@grandparade.co.uk>
2018-05-06 02:55:53 +0200

    ODSC02-3 Pass one more unit tests

--- odsc02/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ odsc02/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -23,9 +23,44 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  property("gen111") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    val k = m+1
+    insert(m,h)
+    insert(k,h)
+    findMin(h) == m
+  }
+
   property("min1") = forAll { a: Int =>
     val h = insert(a, empty)
     findMin(h) == a
   }
+
+
+//  property("min2") = forAll { a: Int =>
+//    val h = insert(a, empty)
+//    deleteMin(h)
+//
+//    isEmpty(h)
+//  }
+
+  property("gen12") = forAll { (h: H) =>
+   if (!isEmpty(h)) {
+     val  m = findMin(h)
+     insert(m-1, h)
+     findMin(h) !=  m
+   }
+    true
+  }
+
+  property("melt") = forAll { (h1:H, h2:H) => {
+    val m1 = findMin(h1)
+    val m2 = findMin(h2)
+    val h = meld(h1,h2)
+    findMin(h)== Math.min(m1,m2)
+  }
+
+  }
+
 
 }
