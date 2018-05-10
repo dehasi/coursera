cdb098b3e5d68e6660b498522acf40ff762813b0
Ravil Galeyev <dehasi@yandex.ru>
2018-05-10 20:05:41 +0200

    ODSC02-4: Add frp bank account implementation

--- odsc02/calculator/src/main/scala/week04/frp/BankAccount.scala
+++ odsc02/calculator/src/main/scala/week04/frp/BankAccount.scala
@@ -1,20 +1,23 @@
 package week04.frp
+
 import calculator.Var
 
 /** Created by Ravil on 10/05/2018. */
 class BankAccount {
-  var balance = Var(0)
+  val balance = Var(0)
 
-  def currentBalance:Int = balance()
+  //  def currentBalance:Int = balance()
 
   def deposit(amount: Int): Unit =
     if (amount > 0) {
-      balance() = balance() + amount
+      val b = balance()
+      balance() = b + amount
     }
 
   def withdraw(amount: Int): Unit = {
     if (0 < amount && amount <= balance()) {
-      balance() = balance() - amount
+      val b = balance()
+      balance() = b - amount
     } else throw new Error("insufficient funds")
   }
 }
---
+++ odsc02/calculator/src/main/scala/week04/frp/accounts.scala
@@ -0,0 +1,19 @@
+package week04.frp
+
+import calculator.Signal
+
+/** Created by Ravil on 10/05/2018. */
+object accounts {
+  def consolidated(accs: List[BankAccount]): Signal[Int] =
+    Signal(accs.map(_.balance()).sum)
+
+  def main(s: Array[String]): Unit = {
+    println("welcome")
+    val a = new BankAccount
+    val b = new BankAccount
+    val c = consolidated(List(a, b))
+    println(c())
+    a deposit 20
+    println(c())
+  }
+}
